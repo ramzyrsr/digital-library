a0675cadcfbf4e21c70f68825ff5d05876ef1b1f
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2025-03-26 02:03:23 +0700

    add get user function

--- backend/internal/repository/user_repository.go
+++ backend/internal/repository/user_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/ramzyrsr/digital-library/internal/entity"
 	"golang.org/x/crypto/bcrypt"
@@ -23,4 +24,17 @@ func (r *UserRepository) CreateUser(user *entity.User) error {
 	_, err = r.DB.Exec(query, user.Email, string(hashedPassword), user.Name, user.Role)
 
 	return err
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*entity.User, error) {
+	user := &entity.User{}
+	query := `SELECT id, email, name, role, created_at FROM users WHERE email = $1`
+	err := r.DB.QueryRow(query, email).Scan(&user.ID, &user.Email, &user.Name, &user.Role, &user.CreatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("user not found")
+		}
+		return nil, err
+	}
+	return user, nil
 }
