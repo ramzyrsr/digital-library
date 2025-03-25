2cc401f0f4302d4cbb045731d85673e059bbc91a
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2025-03-26 02:01:37 +0700

    init function to generate token

---
+++ backend/pkg/jwt_utils.go
@@ -0,0 +1,19 @@
+package pkg
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID int) (string, error) {
+	secret := os.Getenv("JWT_SECRET")
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), // Token expires in 24 hours
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
