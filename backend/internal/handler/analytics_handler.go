098d0348ec4e6d9e2bbe2fba4e4876fd687f2083
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2025-03-26 16:49:33 +0700

    init most borrowed book handler

---
+++ backend/internal/handler/analytics_handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/ramzyrsr/digital-library/internal/middleware"
+	"github.com/ramzyrsr/digital-library/internal/repository"
+)
+
+type AnalyticsHandler struct {
+	AnalyticsRepo *repository.AnalyticsRepository
+}
+
+func (h *AnalyticsHandler) MostBorrowedBooks(c *fiber.Ctx) error {
+	limit, _ := strconv.Atoi(c.Query("limit", "5"))
+
+	books, err := h.AnalyticsRepo.GetMostBorrowedBooks(limit)
+	if err != nil {
+		return middleware.Response(c, fiber.StatusBadRequest, "Failed to retrieve data", err.Error())
+	}
+
+	return middleware.Response(c, fiber.StatusOK, "Success to retrieve data", books)
+}
