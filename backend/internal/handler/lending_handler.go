cc58779672b6b73985b9dea26417725cc4d6bf3e
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2025-03-26 15:19:56 +0700

    init borrow book handler

---
+++ backend/internal/handler/lending_handler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/ramzyrsr/digital-library/internal/entity"
+	"github.com/ramzyrsr/digital-library/internal/middleware"
+	"github.com/ramzyrsr/digital-library/internal/repository"
+)
+
+type LendingHandler struct {
+	LendingRepo *repository.LendingRepository
+}
+
+func (h *LendingHandler) BorrowBook(c *fiber.Ctx) error {
+	var req entity.Lending
+	if err := c.BodyParser(&req); err != nil {
+		return middleware.Response(c, fiber.StatusBadRequest, "Invalid input", err.Error())
+	}
+
+	userID := c.Locals("user_id").(uint)
+	req.CreatedBy = userID
+
+	if err := h.LendingRepo.BorrowBook(&req); err != nil {
+		return middleware.Response(c, fiber.StatusBadRequest, "Failed to borrow book", err.Error())
+	}
+
+	return middleware.Response(c, fiber.StatusCreated, "Success to borrow book", map[string]interface{}{
+		"id": req.ID,
+	})
+}
